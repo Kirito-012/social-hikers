1bb74ab37bef396fdd597a34daa3eb34419b5e03
akshat <11.uknowwho@gmail.com>
2023-07-16 11:39:37 +0530

    fourth commit

--- src/app/components/Featured.js
+++ src/app/components/Featured.js
@@ -11,33 +11,123 @@ const Featured = () => {
 					>
 						Featured On
 					</h1>
-					<div
-						className={`${style["featured__iconholder"]} opacity-80 bg-[#0C4A9D] flex justify-evenly items-center `}
-					>
-						<Image
-							className={style.featured__swiggylogo}
-							src="/Swiggy-logo.png"
-							width={206}
-							height={57}
-						></Image>
-						<Image
-							className={style.featured__realmelogo}
-							src="/realme-logo.png"
-							width={214}
-							height={60}
-						></Image>
-						<Image
-							className={style.featured__mxplayerlogo}
-							src="/MXplayer-logo.png"
-							width={255}
-							height={63}
-						></Image>
-						<Image
-							className={style.featured__whitehilllogo}
-							src="/whitehill-logo.jpg"
-							width={100}
-							height={55}
-						></Image>
+					<div className={style.slider}>
+						<div
+							className={`${style["featured__iconholder"]} opacity-80 bg-[#0C4A9D] flex justify-center gap-12 items-center `}
+						>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__swiggylogo"]}`}
+									src="/Swiggy-logo.png"
+									width={206}
+									height={57}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__realmelogo"]}`}
+									src="/realme-logo.png"
+									width={214}
+									height={60}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__mxplayerlogo"]}`}
+									src="/MXplayer-logo.png"
+									width={255}
+									height={63}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__whitehilllogo"]}`}
+									src="/whitehill-logo.jpg"
+									width={100}
+									height={55}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__patanjalilogo"]}`}
+									src="/patanjali-logo.jpeg"
+									width={150}
+									height={95}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__zee5logo"]}`}
+									src="/Zee5-logo.jpg"
+									width={150}
+									height={95}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__mpllogo"]}`}
+									src="/mpl-logo.png"
+									width={110}
+									height={105}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__swiggylogo"]}`}
+									src="/Swiggy-logo.png"
+									width={206}
+									height={57}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__realmelogo"]}`}
+									src="/realme-logo.png"
+									width={214}
+									height={60}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__mxplayerlogo"]}`}
+									src="/MXplayer-logo.png"
+									width={255}
+									height={63}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__whitehilllogo"]}`}
+									src="/whitehill-logo.jpg"
+									width={100}
+									height={55}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__patanjalilogo"]}`}
+									src="/patanjali-logo.jpeg"
+									width={150}
+									height={95}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__zee5logo"]}`}
+									src="/Zee5-logo.jpg"
+									width={150}
+									height={95}
+								></Image>
+							</div>
+							<div className={style.icon_slider}>
+								<Image
+									className={`${style["logo"]} ${style["featured__mpllogo"]}`}
+									src="/mpl-logo.png"
+									width={110}
+									height={105}
+								></Image>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
